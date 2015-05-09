45a182c80ebb2e5c4dcaa5fc091efe2c9fb29f93
Steven Hopkins <srhopkins@gmail.com>
2015-05-09 12:32:02 -0700

    Update examples Dockerfile

--- dockerfiles/init.sh
+++ dockerfiles/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/opt/mongo*/bin/mongod --quiet --logpath /dev/null &
+/opt/mongo*/bin/mongod --smallfiles --quiet --logpath /dev/null &
 
 [ -f /notebooks/requirements.txt ] && pip install -r /notebooks/requirements.txt
 
