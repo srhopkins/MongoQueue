28bdd8fe9c35e49c974f7cbfd30bccccf6e77977
Steven Hopkins <srhopkins@gmail.com>
2016-02-08 12:44:49 -0700

    Moving to object item model

--- package/mongoku.py
+++ package/mongoku.py
@@ -7,14 +7,35 @@ status_working = {"status": "working"}
 status_error = {"status": "error"}
 status_done = {"status": "done"}
 
-class QueueMaster():
-    def __init__(self, client="localhost", db="MongoQueue", queue="anonymous"):
-        try:
-            self.client = MongoClient(client)
-        except ConnectionFailure:
-            raise Exception("Please provide client information.")
-        self.db = self.client[db]
-        self.queues = self.db.collection_names
+
+class Item(object):
+    def __init__(self, mqueue, _dict):
+        self._mqueue = mqueue
+        self._dict = _dict
+        
+    @property
+    def meta(self):
+        return self._dict["meta"]
+    
+    @property
+    def item(self):
+        return self._dict["item"]
+    
+    def _put(self):
+        self._mqueue.put(self._dict)
+    
+    def queued(self):
+        pass
+    
+    def working(self):
+        pass
+    
+    def exception(self):
+        pass
+    
+    def done(self):
+        pass
+    
     
 class MongoConnection():
     # TODO add support for MongoReplicaSetClient
@@ -36,45 +57,35 @@ class MongoQueue():
         self.queue = self.mongo.queue
         
     def put(self, item):
-        commit = dict(status_queued.items() + item.items())
-        rec = self.queue.insert(commit)
+        rec = self.queue.insert({
+                "meta": status_queued,
+                "item": item
+                })
         
     def puts(self, items):
         commit = [{"meta": status_queued, "item": item} for item in items]
         self.queue.insert(commit, {"ordered": True})
         
-    def get(self, query=status_queued):
+    def get(self, query={"meta.status": "queued"}):
         # Returns one single work item, oldest first.
         # Watch out if you are just trying to view the record; use .find instead.
         # .get will always set status to working
-        return self.queue.find_and_modify(query=query,
-                                          update={"$set": status_working},
+        item = self.queue.find_and_modify(query=query,
+                                          update={"$set": {"meta.status": "working"}},
                                           new=True)
+        return Item(self, item) if item else None
              
-    def gets(self, query=status_queued):
+    def gets(self):
         # Returns a generator to loop through work.
         while True:
-            item = self.get(query)
+            item = self.get()
             if item:
                 yield item
             else:
                 raise StopIteration
-                
-    def done(self, item):
-        item["status"] = "done"
-        self.queue.save(item)
                 
     def find_one(self, query={}):
         return self.queue.find_one(query)
     
     def find(self, query={}):
         return self.queue.find(query)
-    
-    def find_done(self):
-        return self.queue.find(status_done)
-    
-    def find_working(self):
-        return self.queue.find(status_working)
-    
-    def find_queued(self):
-        return self.queue.find(status_queued)
